b7922fcad5ae9700174bc34dda320869a325e3cf
noufanelachola <noufansadiqelachola@gmail.com>
2024-08-08 05:06:19 +0530

    face detection

---
+++ 15-face_detect.py
@@ -0,0 +1,20 @@
+import cv2 as cv
+
+img = cv.imread("Photos\BC.png")
+resized = cv.resize(img,(400,450),cv.INTER_AREA)
+
+gray = cv.cvtColor(resized,cv.COLOR_BGR2GRAY)
+
+haar_cascade = cv.CascadeClassifier("haar_cascade.xml")
+
+faces_rect = haar_cascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors = 3 )
+
+for (x,y,w,g) in faces_rect:
+    cv.rectangle((resized),(x,y),(x+w,y+g),(0,255,0),thickness=2)
+
+cv.imshow("Image",resized)
+cv.imshow("Gray",gray)
+
+print(f"Number of people is = {len(faces_rect)}")
+
+cv.waitKey(0)
