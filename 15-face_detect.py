640f29b2e718060964f36ded56fb6263fc985d58
noufanelachola <noufansadiqelachola@gmail.com>
2024-10-05 08:10:36 +0530

    face recignition

--- 15-face_detect.py
+++ 15-face_detect.py
@@ -1,6 +1,7 @@
 import cv2 as cv
 
-img = cv.imread("Photos\BC.png")
+img = cv.imread("Photos\download.jpg")
+# img = cv.imread("Photos\BC.png")
 resized = cv.resize(img,(400,450),cv.INTER_AREA)
 
 gray = cv.cvtColor(resized,cv.COLOR_BGR2GRAY)
@@ -8,6 +9,7 @@ gray = cv.cvtColor(resized,cv.COLOR_BGR2GRAY)
 haar_cascade = cv.CascadeClassifier("haar_cascade.xml")
 
 faces_rect = haar_cascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors = 3 )
+print(faces_rect)
 
 for (x,y,w,g) in faces_rect:
     cv.rectangle((resized),(x,y),(x+w,y+g),(0,255,0),thickness=2)
